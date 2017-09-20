696f8b09edccdeb9ab5223bf922d72b151d3f708
Adam D. White <adamwhite82@gmail.com>
2017-09-19 21:22:25 -0500

    calling all breeds in search function

--- app/factories/factory-breeds.js
+++ app/factories/factory-breeds.js
@@ -6,17 +6,41 @@ app.factory("breedSearchFactory", function($q, $http, FBCreds) {
     console.log('breed search-bar factory!');
     let getBreedNames = () => {
         return $q((resolve, reject) => {
-            $http.get("data/breed-set.json")
-                .then((itemObject) => {
-                    let itemCollection = itemObject.data;
-                    console.log("breeds from search", itemCollection);
-                    resolve(itemCollection);
+            let allBreedNames = [];
+            Promise.all([
+                    getBreedsBySize('small', allBreedNames),
+                    getBreedsBySize('medium', allBreedNames),
+                    getBreedsBySize('size', allBreedNames)
+                ])
+                .then(() => {
+                    console.log("breeds from search", allBreedNames);
+                    let detailPromises = [];
+                    let allDogDetails = [];
+                    allBreedNames.forEach(function(dogName) {
+                        detailPromises.push($http.get(`https://dogbreed-characteristics.herokuapp.com/details/?dogBreed=${dogName}`))
+                            .then((result) => {
+                                allDogDetails.push(result.data);
+                            });
+                    }, this);
+                    Promise.all(detailPromises)
+                        .then(() => {
+                            resolve(allDogDetails);
+                        });
                 })
                 .catch((error) => {
                     reject(error);
                 });
         });
     };
+
+    const getBreedsBySize = (size, runningList) => {
+        return $http.get(`https://dogbreed-characteristics.herokuapp.com/characteristic/?characteristic=${size}`)
+            .then((results) => {
+                results.data['breeds'].forEach(function(dogName) {
+                    runningList.push(dogName);
+                }, this);
+            });
+    }
 
     // This is the factory for PUSHING the saved breed object to firebase
     const saveBreed = function(object) {
